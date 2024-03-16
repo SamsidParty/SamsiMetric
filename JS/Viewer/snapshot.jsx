ba9a59d27c3aac0339f58b7d3e3dc44e537edd20
Samarth Pradeep <79756986+SamarthMP@users.noreply.github.com>
2024-03-16 07:26:55 +0300

    Implement MessagePack On get_snapshot_data

--- JS/Viewer/snapshot.jsx
+++ JS/Viewer/snapshot.jsx
@@ -10,34 +10,39 @@ var SnapshotTables = {
 }
 
 async function LoadSnapshotRange(from, to) {
-    //Sync Snapshot Range From Server
-    var response = await fetch(Backend, {
-        headers: DefaultHeaders({ "X-Params": JSON.stringify({
-            action: "get_snapshot_data",
-            date_start: from,
-            date_end: to
-        }) })
-    });
+    try {
+        //Sync Snapshot Range From Server
+        var response = await fetch(Backend, {
+            headers: DefaultHeaders({ "X-Params": JSON.stringify({
+                action: "get_snapshot_data",
+                date_start: from,
+                date_end: to
+            }) })
+        });
 
-    if (!response.ok) {
-        if (response.status == 401 || response.status == 424) {
-            window.location.href = "./Login"
+        if (!response.ok) {
+            if (response.status == 401 || response.status == 424) {
+                window.location.href = "./Login"
+            }
+            dataStatus = "error";
+            return;
         }
-        dataStatus = "error";
-        return;
+
+        (await response.json()).data_snapshot.forEach((l_snap) => {
+            var identity = l_snap.MetricID + "_" + l_snap.SnapTime; // Prevents Duplication
+            if (LoadedSnapshots[identity] == undefined) {
+                LoadedSnapshots[identity] = l_snap;
+            }
+            if (SnapshotsFor[l_snap.MetricID] == undefined) {
+                SnapshotsFor[l_snap.MetricID] = {};
+            }
+            SnapshotsFor[l_snap.MetricID][l_snap.SnapTime] = l_snap;
+        });
+        LoadedSnapshotRanges.push(from + "_" + to);
     }
-
-    (await response.json()).data_snapshot.forEach((l_snap) => {
-        var identity = l_snap.MetricID + "_" + l_snap.SnapTime; // Prevents Duplication
-        if (LoadedSnapshots[identity] == undefined) {
-            LoadedSnapshots[identity] = l_snap;
-        }
-        if (SnapshotsFor[l_snap.MetricID] == undefined) {
-            SnapshotsFor[l_snap.MetricID] = {};
-        }
-        SnapshotsFor[l_snap.MetricID][l_snap.SnapTime] = l_snap;
-    });
-    LoadedSnapshotRanges.push(from + "_" + to);
+    catch {
+        //TODO: GUI Error Handler
+    }
 }
 
 function SnapshotAt(metric, time) {
--- PHP/Actions/get_snapshot_data.php
+++ PHP/Actions/get_snapshot_data.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once("./PHP/ThirdParty/vendor/autoload.php");
+
 ViewerOnly();
 
 $result = array();
@@ -7,9 +9,9 @@ $tdata = DB::query("SELECT * FROM data_snapshot WHERE SnapTime BETWEEN %d AND %d
 $result["data_snapshot"] = $tdata;
 
 for ($i = 0; $i < sizeof($result["data_snapshot"]); $i++) {
-    $result["data_snapshot"][$i]["SnapData"] = gzuncompress($result["data_snapshot"][$i]["SnapData"]);
+    $result["data_snapshot"][$i]["SnapData"] = $result["data_snapshot"][$i]["SnapData"];
 }
-
-echo json_encode($result);
+$packer = new MessagePack\Packer();
+echo $packer->pack($result);
 
 ?>
