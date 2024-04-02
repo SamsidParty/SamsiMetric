6d1b89cb3735a88a28a04dc768fe2226ef3476fc
Samarth Pradeep <79756986+SamarthMP@users.noreply.github.com>
2024-04-02 07:24:27 +0300

    Start Implementing Workspace Manager

--- JS/Management/workspacemanager.jsx
+++ JS/Management/workspacemanager.jsx
@@ -1,5 +1,6 @@
 
 var workspaceEditMode = false;
+var manageWorkspaceBackup = null;
 
 RunOnLoad("./JS/Management/workspacemanager.jsx", async () => {
     await LoadDependency("./JS/Management/graphselect.jsx");
@@ -65,21 +66,56 @@ function CreateWorkspace(props) {
 
 function WorkspaceEditor(props)
 {
+    var { DataObject, setDataObject } = React.useContext(DataContext); window.lastDataObject = DataObject;
     var [isOpen, setIsOpen] = React.useState(false);
+
+    var changeColor = (e) => {
+        var ws = ArrayValue(CurrentProject(DataObject).workspaces, "id", props.workspace.id);
+        ws.tag = e.target.value;
+        setDataObject(Object.assign({}, DataObject));
+    }
+    
+    var startEditing = () => {
+        //Create A Backup Of The Current Data
+        //Used To Discard Changes
+        manageWorkspaceBackup = Object.assign({}, ArrayValue(CurrentProject(DataObject).workspaces, "id", props.workspace.id));
+        setIsOpen(true);
+    }
+
+    var apply = async () => {
+        //Send Changes To Server
+        await SyncWorkspaceChanges();
+        setDataObject(Object.assign({}, DataObject));
+        setIsOpen(false);
+    }
+
+    var discard = () => {
+        //Restore From Backup
+        var ws = ArrayIndex(CurrentProject(DataObject).workspaces, "id", props.workspace.id);
+        CurrentProject(DataObject).workspaces[ws] = Object.assign({}, manageWorkspaceBackup);
+        setDataObject(Object.assign({}, DataObject));
+        setIsOpen(false);
+    }
 
     return (
         <>
-            <Button onPress={() => setIsOpen(true)} color={props.workspace.tag} flat auto className="iconButton iconButtonLarge">
+            <Button onPress={startEditing} color={props.workspace.tag} flat auto className="iconButton iconButtonLarge">
                 <i className="ti ti-edit"></i>
             </Button>
-            <Modal width="900px" closeButton open={isOpen} onClose={() => setIsOpen(false)}>
+            <Modal closeButton open={isOpen} onClose={() => setIsOpen(false)}>
                 <Modal.Header>
                     <Text b id="modal-title" size={20}>
                         Edit Workspace
                     </Text>
                 </Modal.Header>
                 <Modal.Body>
-                    
+                    <p style={{ fontSize: "0.875rem", marginBottom: "6px", marginLeft: "4px", letterSpacing: "initial" }} className="nextui-input-block-label">Color</p>
+                    <ColorBar onChange={changeColor} value={props.workspace.tag} data-key={props.workspace.tag} />
+                    <div className="flexx fillx fjend">
+                        <Button auto flat onPress={discard}>Discard</Button>
+                        <NextUI.Spacer x={0.5} />
+                        <Button auto onPress={apply}>Save</Button>
+                    </div>
                 </Modal.Body>
             </Modal>
         </>
