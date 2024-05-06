117ae8528d7b7b587987ddf72a5d9896ea31762f
Samarth Pradeep <79756986+SamarthMP@users.noreply.github.com>
2024-05-06 10:59:52 +0300

    Update Standalone Client

--- Standalone/SamsiMetric.WWW/SamsiMetric/src/App.jsx
+++ Standalone/SamsiMetric.WWW/SamsiMetric/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { ChakraProvider, extendTheme } from '@chakra-ui/react'
+import { ChakraProvider, extendTheme, Button } from '@chakra-ui/react'
 
 window.selectedMode = -1;
 
@@ -8,15 +8,44 @@ const config = {
     useSystemColorMode: true,
 }
 
-const theme = extendTheme({ config })
+const theme = extendTheme({ config });
 
-window.location.href = "http://192.168.100.100:8082/Analytics/Build/Dashboard";
+var serverList = [
+    {
+        name: "Dev Server",
+        url: "https://192.168.100.100:8083/Analytics/Dashboard"
+    }
+]
+
 
 function App() {
+
+    var loadProfile = (server) => {
+        window.location.href = server.url;
+    }
+
     return (
         <>
             <ChakraProvider theme={theme}>
-                <h1>Test</h1>
+                <div className="setupContainer">
+                    <div className="setupModal selectServerModal">
+                        <p className="mobileOnly mobileIcon">&#xeb1f;</p>
+                        <h1>Select Profile</h1>
+                        <div className="serverList">
+                            {
+                                serverList.length > 0 ?
+                                (serverList.map((l_server, l_index) => {
+                                    return (
+                                        <Button onClick={() => loadProfile(l_server)} className="serverListItem" key={l_index}>
+                                            <h2>{l_server.name}</h2>
+                                        </Button>
+                                    )
+                                })) : 
+                                (<p>No Profiles Available, Connect To A Server To Create One</p>)
+                            }
+                        </div>
+                    </div>
+                </div>
             </ChakraProvider>
         </>
 
