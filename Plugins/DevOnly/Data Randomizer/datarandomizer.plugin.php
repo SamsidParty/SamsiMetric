8556a5228780fafc0891cca4c5a47b3596c45138
Samarth Pradeep <79756986+SamarthMP@users.noreply.github.com>
2024-05-10 09:53:24 +0300

    Update Data Randomizer Plugin

--- Plugins/DevOnly/Data Randomizer/datarandomizer.plugin.php
+++ Plugins/DevOnly/Data Randomizer/datarandomizer.plugin.php
@@ -1,5 +1,13 @@
 <?php
 
+//This File Should Only Run When Included By The Snapshot Writer
+$included_files = get_included_files();
 
+foreach ($included_files as $filename) {
+    if (str_ends_with($filename, "PHP/MetricWriters/snapshot.php")) {
+        //Randomize The Data In The Database
+        DB::query("UPDATE data_total SET Total = FLOOR(1 + RAND() * 300)");
+    }
+}
 
 ?>
